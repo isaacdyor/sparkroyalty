3fa193cfea5c24afb333f4f7dd4a071dcde970b2
Isaac Dyor <isaac@dyor.com>
2023-11-09 23:34:53 -0800

    fake commut

--- src/components/shared/messageListener.tsx
+++ src/components/shared/messageListener.tsx
@@ -34,165 +34,165 @@ const MessageListener: React.FC = () => {
   });
 
   // subscribe to pusher
-  useEffect(() => {
-    const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
-      cluster: "us3",
-    });
-    if (isLoaded) {
-      if (user?.unsafeMetadata.active === ActiveType.FOUNDER) {
-        pusher.subscribe(toPusherKey(`founder:${user?.id}`));
-      } else if (user?.unsafeMetadata.active === ActiveType.INVESTOR) {
-        pusher.subscribe(toPusherKey(`investor:${user?.id}`));
-      }
-    }
+  // useEffect(() => {
+  //   const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
+  //     cluster: "us3",
+  //   });
+  //   if (isLoaded) {
+  //     if (user?.unsafeMetadata.active === ActiveType.FOUNDER) {
+  //       pusher.subscribe(toPusherKey(`founder:${user?.id}`));
+  //     } else if (user?.unsafeMetadata.active === ActiveType.INVESTOR) {
+  //       pusher.subscribe(toPusherKey(`investor:${user?.id}`));
+  //     }
+  //   }
 
-    const newNotificationHandler = (notification: NotificationType) => {
-      const date = new Date(notification.createdAt);
-      const newNotification = {
-        ...notification,
-        createdAt: date,
-      };
-      setNotifications((prevNotifications) => {
-        const updatedNotifications = [
-          ...(prevNotifications || []),
-          newNotification,
-        ];
-        return updatedNotifications;
-      });
-      void ctx.notifications.getCurrent.invalidate();
-      sendToast(notification.subject, notification.content);
-    };
+  //   const newNotificationHandler = (notification: NotificationType) => {
+  //     const date = new Date(notification.createdAt);
+  //     const newNotification = {
+  //       ...notification,
+  //       createdAt: date,
+  //     };
+  //     setNotifications((prevNotifications) => {
+  //       const updatedNotifications = [
+  //         ...(prevNotifications || []),
+  //         newNotification,
+  //       ];
+  //       return updatedNotifications;
+  //     });
+  //     void ctx.notifications.getCurrent.invalidate();
+  //     sendToast(notification.subject, notification.content);
+  //   };
 
-    const newConversationHandler = async (conversation: ConversationType) => {
-      setConversations((prevConversations) => {
-        const prevConversationsArray = prevConversations ?? [];
+  //   const newConversationHandler = async (conversation: ConversationType) => {
+  //     setConversations((prevConversations) => {
+  //       const prevConversationsArray = prevConversations ?? [];
 
-        return [conversation, ...prevConversationsArray];
-      });
-      if (user?.unsafeMetadata.active === ActiveType.FOUNDER) {
-        sendToast(
-          conversation.investorName!,
-          conversation.messages![0]!.content,
-          conversation.investorImageUrl
-        );
-      } else if (user?.unsafeMetadata.active === ActiveType.INVESTOR) {
-        sendToast(
-          conversation.founderName!,
-          conversation.messages![0]!.content,
-          conversation.founderImageUrl
-        );
-      }
-      const response = await refetch();
-      return response;
-    };
+  //       return [conversation, ...prevConversationsArray];
+  //     });
+  //     if (user?.unsafeMetadata.active === ActiveType.FOUNDER) {
+  //       sendToast(
+  //         conversation.investorName!,
+  //         conversation.messages![0]!.content,
+  //         conversation.investorImageUrl
+  //       );
+  //     } else if (user?.unsafeMetadata.active === ActiveType.INVESTOR) {
+  //       sendToast(
+  //         conversation.founderName!,
+  //         conversation.messages![0]!.content,
+  //         conversation.founderImageUrl
+  //       );
+  //     }
+  //     const response = await refetch();
+  //     return response;
+  //   };
 
-    const updateRecieverMessages = (message: MessageType) => {
-      let newMessage: MessageType;
-      if (message.senderType === AccountType.FOUNDER) {
-        newMessage = {
-          content: message.content,
-          createdAt: new Date(),
-          id: nanoid(),
-          investorId: message.investorId,
-          founderId: message.founderId,
-          senderType: AccountType.FOUNDER,
-          senderName: user!.fullName!,
-          conversationId: message.conversationId,
-        };
-      } else {
-        newMessage = {
-          content: message.content,
-          createdAt: new Date(),
-          id: nanoid(),
-          investorId: message.investorId,
-          founderId: message.founderId,
-          senderType: AccountType.INVESTOR,
-          senderName: user!.fullName!,
-          conversationId: message.conversationId,
-        };
-      }
+  //   const updateRecieverMessages = (message: MessageType) => {
+  //     let newMessage: MessageType;
+  //     if (message.senderType === AccountType.FOUNDER) {
+  //       newMessage = {
+  //         content: message.content,
+  //         createdAt: new Date(),
+  //         id: nanoid(),
+  //         investorId: message.investorId,
+  //         founderId: message.founderId,
+  //         senderType: AccountType.FOUNDER,
+  //         senderName: user!.fullName!,
+  //         conversationId: message.conversationId,
+  //       };
+  //     } else {
+  //       newMessage = {
+  //         content: message.content,
+  //         createdAt: new Date(),
+  //         id: nanoid(),
+  //         investorId: message.investorId,
+  //         founderId: message.founderId,
+  //         senderType: AccountType.INVESTOR,
+  //         senderName: user!.fullName!,
+  //         conversationId: message.conversationId,
+  //       };
+  //     }
 
-      setConversations((prevConversations) => {
-        const prevConversationsArray = prevConversations ?? [];
+  //     setConversations((prevConversations) => {
+  //       const prevConversationsArray = prevConversations ?? [];
 
-        const unsortedArray = prevConversationsArray.map((conversation) => {
-          if (conversation.id === newMessage.conversationId) {
-            return {
-              ...conversation,
-              lastMessageAt: new Date(),
-              messages: [...conversation.messages!, newMessage],
-              founderSeen:
-                message.senderType === AccountType.INVESTOR &&
-                selectedConversation?.id !== message.conversationId
-                  ? false
-                  : conversation.founderSeen,
-              investorSeen:
-                message.senderType === AccountType.FOUNDER &&
-                selectedConversation?.id !== message.conversationId
-                  ? false
-                  : conversation.investorSeen,
-            };
-          }
-          return conversation;
-        });
-        return sortConversation(unsortedArray);
-      });
-    };
+  //       const unsortedArray = prevConversationsArray.map((conversation) => {
+  //         if (conversation.id === newMessage.conversationId) {
+  //           return {
+  //             ...conversation,
+  //             lastMessageAt: new Date(),
+  //             messages: [...conversation.messages!, newMessage],
+  //             founderSeen:
+  //               message.senderType === AccountType.INVESTOR &&
+  //               selectedConversation?.id !== message.conversationId
+  //                 ? false
+  //                 : conversation.founderSeen,
+  //             investorSeen:
+  //               message.senderType === AccountType.FOUNDER &&
+  //               selectedConversation?.id !== message.conversationId
+  //                 ? false
+  //                 : conversation.investorSeen,
+  //           };
+  //         }
+  //         return conversation;
+  //       });
+  //       return sortConversation(unsortedArray);
+  //     });
+  //   };
 
-    const newMessageHandler = (message: MessageType) => {
-      if (
-        (user?.unsafeMetadata.active === ActiveType.FOUNDER &&
-          message.senderType === AccountType.FOUNDER) ??
-        (user?.unsafeMetadata.active === ActiveType.INVESTOR &&
-          message.senderType === AccountType.INVESTOR)
-      ) {
-        return;
-      } else {
-        // send toast
-        console.log("hello");
-        if (message.conversationId != selectedConversation?.id) {
-          sendToast(message.senderName!, message.content, message.imageUrl);
-        }
-        updateRecieverMessages(message);
-        markNotRead(message);
-      }
-    };
+  //   const newMessageHandler = (message: MessageType) => {
+  //     if (
+  //       (user?.unsafeMetadata.active === ActiveType.FOUNDER &&
+  //         message.senderType === AccountType.FOUNDER) ??
+  //       (user?.unsafeMetadata.active === ActiveType.INVESTOR &&
+  //         message.senderType === AccountType.INVESTOR)
+  //     ) {
+  //       return;
+  //     } else {
+  //       // send toast
+  //       console.log("hello");
+  //       if (message.conversationId != selectedConversation?.id) {
+  //         sendToast(message.senderName!, message.content, message.imageUrl);
+  //       }
+  //       updateRecieverMessages(message);
+  //       markNotRead(message);
+  //     }
+  //   };
 
-    const markNotRead = (message: MessageType) => {
-      if (selectedConversation?.id === message.conversationId) {
-        return;
-      } else {
-        if (message.senderType === AccountType.FOUNDER) {
-          markNotReadMutation({
-            conversationId: message.conversationId,
-            accountType: AccountType.INVESTOR,
-          });
-        } else if (message.senderType === AccountType.INVESTOR) {
-          markNotReadMutation({
-            conversationId: message.conversationId,
-            accountType: AccountType.FOUNDER,
-          });
-        }
-      }
-    };
+  //   const markNotRead = (message: MessageType) => {
+  //     if (selectedConversation?.id === message.conversationId) {
+  //       return;
+  //     } else {
+  //       if (message.senderType === AccountType.FOUNDER) {
+  //         markNotReadMutation({
+  //           conversationId: message.conversationId,
+  //           accountType: AccountType.INVESTOR,
+  //         });
+  //       } else if (message.senderType === AccountType.INVESTOR) {
+  //         markNotReadMutation({
+  //           conversationId: message.conversationId,
+  //           accountType: AccountType.FOUNDER,
+  //         });
+  //       }
+  //     }
+  //   };
 
-    pusher.bind("new-notification", newNotificationHandler);
-    pusher.bind("new-conversation", newConversationHandler);
-    pusher.bind("new-message", newMessageHandler);
+  //   pusher.bind("new-notification", newNotificationHandler);
+  //   pusher.bind("new-conversation", newConversationHandler);
+  //   pusher.bind("new-message", newMessageHandler);
 
-    return () => {
-      console.log("unsubscribing");
-      if (user?.unsafeMetadata.active === ActiveType.FOUNDER) {
-        pusher.unsubscribe(toPusherKey(`founder:${user?.id}`));
-      } else if (user?.unsafeMetadata.active === ActiveType.INVESTOR) {
-        pusher.unsubscribe(toPusherKey(`investor:${user?.id}`));
-      }
-      pusher.unbind("new-notification", newNotificationHandler);
-      pusher.unbind("new-conversation", newConversationHandler);
-      pusher.unbind("new-message", newMessageHandler);
-      pusher.disconnect();
-    };
-  }, [user, isLoaded, selectedConversation]);
+  //   return () => {
+  //     console.log("unsubscribing");
+  //     if (user?.unsafeMetadata.active === ActiveType.FOUNDER) {
+  //       pusher.unsubscribe(toPusherKey(`founder:${user?.id}`));
+  //     } else if (user?.unsafeMetadata.active === ActiveType.INVESTOR) {
+  //       pusher.unsubscribe(toPusherKey(`investor:${user?.id}`));
+  //     }
+  //     pusher.unbind("new-notification", newNotificationHandler);
+  //     pusher.unbind("new-conversation", newConversationHandler);
+  //     pusher.unbind("new-message", newMessageHandler);
+  //     pusher.disconnect();
+  //   };
+  // }, [user, isLoaded, selectedConversation]);
   return <></>;
 };
 
--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -7,18 +7,17 @@ const MessageListener: React.FC = () => {
     const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
       cluster: "us3",
     });
-    console.log("subscribing");
-    pusher.subscribe("channel");
+
+    const channel = pusher.subscribe("channel");
 
     const handler = () => {
       console.log("received event");
       setCount((prev) => prev + 1);
     };
 
-    pusher.bind("event", handler);
+    channel.bind("event", handler);
 
     return () => {
-      console.log("unsubscribing");
       pusher.unsubscribe("channel");
       pusher.unbind("event", handler);
       pusher.disconnect();
